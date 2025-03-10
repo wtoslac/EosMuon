3051f99b9747a5c42026038a5cbf9343f0048cf9
wtoslac <wto@yoga716>
2025-03-10 10:33:54 -0700

    Prototype of EosMuonTrigger code

--- ME-XU5-2EG-1I-D11E/Mercury_XU5_PE1.srcs/sources_1/new/EosMuonTrigger.v
+++ ME-XU5-2EG-1I-D11E/Mercury_XU5_PE1.srcs/sources_1/new/EosMuonTrigger.v
@@ -39,9 +39,11 @@ module EosMuonTrigger(
     //assign IOA[20] = ~FMCP[0]; //PIN 1 on Box
     //assign IOA[16] = ~FMCN[3];  // Pin 15 on Box
     //assign IOA[14] = !FMCP[0] & !FMCN[3];
- 
+    
+    wire TopHit, MidHit, BtmHit, BarHit, TripleHits, TopMidHits;
     // The top (top layer on top of the water tank) paddles has 10 channels make them IOA[9:0] 
-    assign TopHit = |IOA[3:0]; // |IOA[9:0];// OR reduction operator
+    //assign TopHit = |OA[9:0];// OR reduction operator
+    assign TopHit = |IOA[3:0]; // Limit to 4 ch for easy testing.
     assign LED[0] = ~TopHit;
     
     // The mid (bottom layer on top of the water tank) paddles has 20 channels make them IOA[25:10] (16-ch) , IOB[3:0] 
@@ -60,81 +62,8 @@ module EosMuonTrigger(
     // Only LED*_N_PL, where * = 1,2,3 are mapped.
     // So what's LED0_N_PL for??
     
-   
+    assign TripleHits = (TopHit & MidHit & BtmHit) | (TopHit & MidHit & BarHit);
+    assign TopMidHits = (TopHit & MidHit) | (~BtmHit & ~ BarHit); 
     
-    
-    
-    //wire Clk1Hz;
-    //SlowClock SlowClock_i(Clk100,Clk1Hz);
-    //always @(posedge Clk1Hz) begin
-    
-    /*
-        for (i = 0; i < 8; i = i + 1) begin
-           if (IOA[i]) 
-            nHits = nHits + 1;
-        end
-    end
-     
-    assign LED[0] = ~nHits[0];
-    assign LED[1] = ~nHits[1];
-    assign LED[2] = ~nHits[2];
-    */
-    
-/*   
-    integer i; 
-    reg [6:0] nHits; 
-    always @(*) begin
-        nHits = 0;       
-        for (i = 0; i < 35; i = i + 1) begin
-            if (FMCN[i])
-                nHits = nHits + 1;
-            if (FMCP[i])
-                nHits = nHits + 1;
-            if (i < 25) begin
-                if (IOA[i])
-                    nHits = nHits + 1;
-                if (IOB[i])
-                    nHits = nHits + 1;
-            end
-            if(i < 7) begin
-                if (IOD[i])
-                    nHits = nHits + 1;
-            end         
-        end
-    end
-    assign LED[0] = ~nHits[0]; // Single Hit
-    assign LED[1] = ~nHits[1]; // Double Hits
-    assign LED[2] = (~nHits[0] & ~nHits[1]); //Triple Hits
-*/
-    //assign IOC[0] = ~nHits[0];
-    //assign IOC[1] = ~nHits[1];
-    //assign IOC[2] = ~nHits[2];
-    //assign LED[0] = ~nHits[0];
-    //assign LED[1] = ~nHits[1];
-    //assign LED[2] = ~nHits[2];
-    //assign IOC[3] = Clk1Hz;
-    //assign IOC[4] = ~Clk1Hz;//IOA[17];//nHits[1]; 
-    //assign LED[0] = Clk1Hz; //nHits[1];
-    //assign LED[1] = ~Clk1Hz; //nHits[1];
     
 endmodule
-/*
-module SlowClock(
-    input Clk100,     // 100 MHz input clock
-    output reg Clk1Hz    // 1 Hz output clock
-    );   
-    // Declare a counter variable to divide the clock
-    reg [26:0] counter;  // 27 bits are enough to count up to 100,000,000
-
-    // Always block to handle clock division
-    always @(posedge Clk100) begin
-        if (counter == 50_000_000) begin
-            counter <= 0;
-            Clk1Hz <= ~Clk1Hz;  // Toggle the 1 Hz clock
-        end 
-        else begin
-            counter <= counter + 1;  // Increment counter
-        end
-    end
-endmodule
-*/
